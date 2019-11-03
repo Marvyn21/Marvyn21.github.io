d8c7b8ef749480ef7eba77d7af7865c5a417fdb5
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2019-11-03 10:41:54 +0300

    Design the landing page

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
 ## Description
-This is my personal website
+This is my professional profile
 
 <img src="https://res.cloudinary.com/dw675k0f5/image/upload/v1571866550/storo/Screenshot_from_2019-10-24_00-35-29.png" />
-
-### Stacks used
- 
-    React
-    Kendo React
-    HTML/CSS
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -6,32 +6,25 @@ export class Welcome extends Component {
         return (
             <section className="intro text-center" id="title-section">
                 <div className="title-text col-sm-10 col-sm-12 col-lg-12">
-                    <div>
-                        <img src="https://res.cloudinary.com/dw675k0f5/image/upload/v1571944755/DSC_0448.jpg"
-                            width="200"
-                            height="200"
-                            className="rounded-circle mx-auto d-block img-responsive"
-                            alt="" />
-                        <div class="rank-label-container">
-                            <label htmlFor="file-upload" class="rank-label">
-                                <img src="https://res.cloudinary.com/dw675k0f5/image/upload/v1570648506/flag.png" width="20" alt="" />
-                            </label>
-                        </div>
+                    
+                    <div className="bb mx-auto d-block">
                     </div>
-                    <i>Oh! Hello, Nice to meet you</i>
-                    <h1 className="title">Hi there, &nbsp;
-          <label class="typewrite" data-period="2000" data-type='[ "Am James Savali.", "Am passionate.", "Am Creative." ]'>
-                            <span class="wrap"></span>
+                    <div style={{margin: "12%"}}></div>
+                    <i className="font-italic">Hello there, </i> <br></br>
+                    <i className="font-italic text-break">You have reached one, </i>
+                    <h1 className="title">Hesbon Kiptoo Maiyo, &nbsp;
+          <label className="typewrite" data-period="2000" data-type='[ "I am passionate","i care about results" ]'>
+                            <span className="wrap"></span>
                         </label></h1>
                         <div className="col-md-4 text-center mt-2 mx-auto">
-                                <i className="fas fa-phone fa-sm"></i>&nbsp;Mobile No: +254708197333<br></br>
-                                <i class="far fa-envelope fa-sm"></i>&nbsp;Email: savalijey@gmail.com
+                                <i className="fas fa-phone fa-sm"></i>&nbsp;Mobile No: +254-727-737-299<br></br>
+                                <i className="far fa-envelope fa-sm"></i>&nbsp;Email: hesbonkiptoo5600@gmail.com
                             </div>
                         <div className="col-md-4 text-center mt-2 mx-auto">
-                                <a href="https://github.com/dev-jey"  target="_blank" rel="noopener noreferrer" className="text-dark"><i class="fab fa-github-square fa-4x p-2" style={{color: 'silver'}}></i></a>
+                                <a href="https://github.com/Hesbon5600"  target="_blank" rel="noopener noreferrer" className="text-dark"><i className="fab fa-github-square fa-4x p-2" style={{color: 'silver'}}></i></a>
 
-                                <a href="https://www.linkedin.com/in/devjey/"  target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin fa-4x p-2"></i></a>
-                                <a href="https://stackoverflow.com/users/10687724/jey-savali/"  target="_blank" rel="noopener noreferrer"><i class="fab fa-stack-overflow fa-4x p-2"  style={{color: 'orange'}}></i></a>
+                                <a href="https://www.linkedin.com/in/hesbon-kiptoo-maiyo/"  target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-4x p-2"></i></a>
+                                <a href="https://stackoverflow.com/users/6531903/hesbon"  target="_blank" rel="noopener noreferrer"><i className="fab fa-stack-overflow fa-4x p-2"  style={{color: 'orange'}}></i></a>
                                 
                             </div>
                 </div>
