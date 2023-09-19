1d1376401c979d20d04ed9f35b9b69d7b669e2b8
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-09-19 11:15:09 +0300

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-## Description
-This is my professional profile
-
-<img src="https://res.cloudinary.com/dp5j2kwic/image/upload/v1692778366/ry9ibkijiyfhthlrwrn3.png" />
---
+++ precache-manifest.c5adc748cb7b2959f7b6afa27dd0ee93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ad5713d69ff608546bcd11b8d6dfdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "149b189e81bc69e052df",
+    "url": "/static/css/main.24a42cc3.chunk.css"
+  },
+  {
+    "revision": "e4b79ed05d9c4acc6661",
+    "url": "/static/js/2.164bbf0f.chunk.js"
+  },
+  {
+    "revision": "72c2168a992bae65033dc2d74bcc3eb6",
+    "url": "/static/js/2.164bbf0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "149b189e81bc69e052df",
+    "url": "/static/js/main.a8e78816.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a408b61d9210dd0798129591604ed527",
+    "url": "/static/media/New image.a408b61d.png"
+  }
+]);
